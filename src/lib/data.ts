4acb4ca754bd9da046d75a84ed6fc9fd92b2b7a9
Chandana <chanduchelliboyina3@gmail.com>
2026-07-07 08:58:14 +0530

    Move food images to public assets and update image paths

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,6 @@
 import crispycornImage from "@/assets/premium_photo-1680118540055-aa9f6ce1d93d.avif";
 
-const harabharakebabImage = "/istockphoto-1024552618-612x612.jpg";
-
+const harabharakebabImage = "/assets/istockphoto-1024552618-612x612.jpg";
 export type Category =
   | "Starters"
   | "Soups"
